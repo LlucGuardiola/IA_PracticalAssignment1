cc58fb78c636120bd28cbfff6a4805d878bdc521
Jan <jrevillam@edu.tecnocampus.cat>
2026-02-18 22:51:19 +0100

    commit

---
+++ Assets/PracticalAssignment1/Scripts/FishSpawner.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour
+{
+    private Camera cam;
+    public GameObject fishPrefab;
+
+    void Start()
+    {
+        cam = Camera.main;
+        //fishPrefab = Resources.Load<GameObject>("FISH"); no esta a aquesta ruta
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) // click dret
+        {
+            var position = cam.ScreenToWorldPoint(Input.mousePosition);
+            position.z = 0;
+            GameObject fish = GameObject.Instantiate(fishPrefab);
+            fish.transform.position = position;
+            fish.transform.Rotate(0, 0, Random.value * 360);
+        }
+    }
+}
