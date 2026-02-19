90edd429202283f0ce7ca3825c625962cd7ce72b
jbatllec <janbatlle@gmail.com>
2026-02-19 22:28:27 +0100

    Flocking

---
+++ Assets/PracticalAssignment1/Scripts/Fish_BLACKBOARD.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+
+public class Fish_BLACKBOARD : MonoBehaviour
+{
+    //[Header("Two point wandering")]
+    public GameObject shark;
+
+    public float sharkRadious = 20.0f;
+
+    //[Header("Seed colecting")]
+  
+    void Start()
+    {
+       
+    }
+
+   
+}
