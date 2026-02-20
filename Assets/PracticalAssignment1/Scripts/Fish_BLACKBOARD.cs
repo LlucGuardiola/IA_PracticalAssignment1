288572a565852eedfe0364cfeaa56eec1e04fee6
apolo1516 <pueblopalote@gmail.com>
2026-02-20 11:55:31 +0100

    Commit

--- Assets/PracticalAssignment1/Scripts/Fish_BLACKBOARD.cs
+++ Assets/PracticalAssignment1/Scripts/Fish_BLACKBOARD.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Fish_BLACKBOARD : MonoBehaviour
 {
     //[Header("Two point wandering")]
-
+    [Header("Detection Settings")]
     public float sharkNearRadious = 20.0f;
 
 
